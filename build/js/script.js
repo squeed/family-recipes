bd5bd0089a463e2a8a0194356f069f6837ae3335
Casey Callendrello <cdc@redhat.com>
2018-12-24 15:04:58 -0500

    Add build to git, because we've had a looooot of eggnog.

---
+++ build/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	console.log("Ready");
+	$(".popup .closebutton").click(function(){
+		$(".popup").hide();
+		$(".fade").hide();
+	})
+});
---
+++ build/precache-manifest.9e0bd012dfbe9205f36a8a042df13f11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06437a2d0b3877862346",
+    "url": "/xmas/2018/static/js/runtime~main.06437a2d.js"
+  },
+  {
+    "revision": "421a550148ea201c570f",
+    "url": "/xmas/2018/static/js/main.421a5501.chunk.js"
+  },
+  {
+    "revision": "59d12e3e30200dd9ba18",
+    "url": "/xmas/2018/static/js/1.59d12e3e.chunk.js"
+  },
+  {
+    "revision": "421a550148ea201c570f",
+    "url": "/xmas/2018/static/css/main.c38e3d26.chunk.css"
+  },
+  {
+    "revision": "68477a344af6eb6b29dcb51fd5c1757b",
+    "url": "/xmas/2018/index.html"
+  }
+];
